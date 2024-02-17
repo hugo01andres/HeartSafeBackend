69ee06aef287f7b1d0ccb8cde8d72912229dadce
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-02-17 17:47:57 +0000

    Preparando arquitectura

---
+++ backend/app/app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_cors import CORS
+from flasgger import Swagger
+from flask_restx import Api
+from flask_injector import FlaskInjector
+# pip install flask-injector
