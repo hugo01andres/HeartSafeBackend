c4d9a01699f377e12df0f8c6ea58556d6fe0a0e7
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-02-24 15:47:35 +0000

    Comento el main

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ def index():
     return redirect('/apidocs')
 
 
-@app.route('/read-file')
-def read_file():
-    spark = SparkSession.builder.appName("Flask and Spark: ").master('spark://spark-master:7077').getOrCreate()
+# @app.route('/read-file')
+# def read_file():
+#     spark = SparkSession.builder.appName("Flask and Spark: ").master('spark://spark-master:7077').getOrCreate()
     
-    try:
-        df = spark.read.csv('file:////home/data/starbucks.csv', header=True, inferSchema=True)
-        return str(df)
-    except Exception as e:   
-        print(e)
-        return 'File not found!', 404   
+#     try:
+#         df = spark.read.csv('file:////home/data/starbucks.csv', header=True, inferSchema=True)
+#         return str(df)
+#     except Exception as e:   
+#         print(e)
+#         return 'File not found!', 404   
 
 # Inicia la aplicacion
 if __name__ == '__main__':
