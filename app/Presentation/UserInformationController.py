209205e0b38ef626aff5451731e5e14ecf99f236
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-02-27 17:25:57 +0000

    Endpoints documentados

--- app/Presentation/UserInformationController.py
+++ app/Presentation/UserInformationController.py
@@ -35,6 +35,23 @@ class RunAnalysis(Resource):
     @api.expect(UserInformation)
     @api.marshal_with(GetPredictionResponse)
     def post(self):
+        """
+        Calcular la prediccion de muerte de un paciente por ataque al corazón
+
+        Este endpoint recibe la información de un paciente y devuelve la probabilidad 
+        de que el paciente muera por un ataque al corazón
+
+        Parametros:
+           Revisar el UserInformationDTO en especial UserInformation
+        
+        Devuelve:
+            - death_prediction: La probabilidad de que el paciente muera por un ataque al corazón
+
+        Ejemplo:
+            {
+                "death_prediction": "47%"
+            }
+        """
         death_prediction = self.spark_services.get_prediction(**api.payload)
         print("Predicción Controller: ", death_prediction)
         # TODO: Retornar death_prediction
@@ -42,7 +59,7 @@ class RunAnalysis(Resource):
 
 
 
-
+"""Este controlador se encarga de obtener la información de las graficas y las recetas saludables y ejercicios saludables"""
 @api.route('/analysispdf', strict_slashes=False)
 class AnalysisPDF(Resource):
     @inject
@@ -54,8 +71,26 @@ class AnalysisPDF(Resource):
         # Obtenemos un pdf con la información
     @api.expect(UserInformation)
     def post(self):
-        graficas = self.spark_services.get_graficas()
-        healthy_recipes = self.ia_services.get_healthy_recipes(**api.payload)
-        healthy_exercises = self.ia_services.get_healthy_exercises(**api.payload)
-        pdf = self.ia_services.make_pdf(graficas, healthy_recipes, healthy_exercises)
+        """
+        Generar un PDF con la información de las graficas y las recetas saludables y ejercicios saludables
+
+        Este endpoint recibe la información de un paciente y devuelve un PDF 
+        con la información de las graficas y las recetas saludables y ejercicios 
+        saludables
+
+        Parametros:
+              Revisar el UserInformationDTO en especial UserInformation
+        
+        Devuelve:
+            - pdf: El PDF con la información de las graficas y las recetas saludables y ejercicios saludables
+
+        Ejemplo:
+            {
+                "pdf": "JVBERi0xLjQKJZOMi54gUmVwb3J0TGFiIEdlbmVyYXRlZCBQREYgZG9jdW1lbnQgaHR0cDovL3d3dy5yZXBvcnRsYWIuY29tCjEgMCBvYmoKPDwKL0YxIDIgMCBSIC9GMiAzIDAgUgo+PgplbmRvYmoKMiAwIG9iago8PAovQmF"
+            }
+        """
+        graficas = self.spark_services.get_graficas() #TODO: Emiliano y Raul la estan haciendo
+        healthy_recipes = self.ia_services.get_healthy_recipes(**api.payload) # TODO: Hugo la esta haciendo
+        healthy_exercises = self.ia_services.get_healthy_exercises(**api.payload) # TODO: Hugo la esta haciendo
+        pdf = self.ia_services.make_pdf(graficas, healthy_recipes, healthy_exercises) # TODO: Completarla, solamente esta la estructura
         return {'pdf': pdf}, 200
