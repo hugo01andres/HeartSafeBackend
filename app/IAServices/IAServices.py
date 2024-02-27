49059dfa4e8d21b4daf57f37fa61290235bf19dd
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-02-27 16:45:09 +0000

    Agrego un ejemplo de pdf

--- app/IAServices/IAServices.py
+++ app/IAServices/IAServices.py
@@ -1,4 +1,9 @@
-class IA:
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
+
+class IAServices:
     def __init__(self):
         print("IA.__init__")
     
@@ -9,3 +14,71 @@ class IA:
     def get_healthy_exercises(self, age, anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine):
         print("IA.get_healthy_exercises")
         return ("Ejercicio 1", "Ejercicio 2", "Ejercicio 3")
+
+    def make_pdf(self, graficas, healthy_recipes, healthy_exercises):
+        print("IA.make_pdf")
+        # Define el documento PDF y el nombre del archivo
+        pdf_name = "Analisis_de_paro_cardiaco.pdf"
+        document = SimpleDocTemplate(pdf_name, pagesize=letter)
+        flowables = []
+        styles = getSampleStyleSheet()
+
+        # Título del documento
+        title = "<h1>Análisis de paro cardíaco</h1>"
+        flowables.append(Paragraph(title, styles['Title']))
+
+        # Descripción
+        description = "<para>Basándonos en la siguiente información fue como obtuvimos la predicción de que te tienes que cuidar</para>"
+        flowables.append(Paragraph(description, styles['Normal']))
+        flowables.append(Spacer(1, 12))
+
+        # Incluyendo gráficas e imágenes
+        for i in range(1, 6):
+            # Añadir la gráfica
+            grafica = f"<para>Gráfica {i}</para>"
+            # graph = f"grafica_{i}.png"  # Reemplaza esto con la ruta a tus archivos de imagen
+            # flowables.append(Image(graph, width=400, height=200))
+            # flowables.append(Spacer(1, 12))
+            flowables.append(Paragraph(grafica, styles['Normal']))
+
+            # Añadir la descripción de la gráfica
+            graph_description = f"<para>Explicación: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</para>"
+            flowables.append(Paragraph(graph_description, styles['Normal']))
+            flowables.append(Spacer(1, 12))
+
+        # Añadir una página de separación si es necesario
+        flowables.append(PageBreak())
+
+        # Título de la segunda página
+        title_plan_alimentacion = "<h1>Plan de alimentación</h1>"
+        flowables.append(Paragraph(title_plan_alimentacion, styles['Title']))
+
+        # Descripción del plan de alimentación
+        description_plan_alimentacion = "<para>De acuerdo a lo analizado te damos este plan de alimentación:</para>"
+        flowables.append(Paragraph(description_plan_alimentacion, styles['Normal']))
+        flowables.append(Spacer(1, 12))
+
+        # Contenido del plan de alimentación
+        for i in range(5):  # Asumiendo que hay 5 párrafos de contenido
+            paragraph_plan_alimentacion = "<para>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar pellentesque habitant morbi tristique senectus et.</para>"
+            flowables.append(Paragraph(paragraph_plan_alimentacion, styles['Normal']))
+            flowables.append(Spacer(1, 12))
+
+        # Título de la sección de ejercicio
+        title_plan_ejercicio = "<h1>Plan de ejercicios</h1>"
+        flowables.append(Paragraph(title_plan_ejercicio, styles['Title']))
+
+        # Descripción del plan de ejercicios
+        description_plan_ejercicio = "<para>De acuerdo a lo analizado te damos este plan de alimentación:</para>"  # Aquí parece haber un error en la imagen, probablemente debería decir 'plan de ejercicios'
+        flowables.append(Paragraph(description_plan_ejercicio, styles['Normal']))
+        flowables.append(Spacer(1, 12))
+
+        # Contenido del plan de ejercicios
+        for i in range(5):  # Asumiendo que hay 5 párrafos de contenido
+            paragraph_plan_ejercicio = "<para>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar pellentesque habitant morbi tristique senectus et.</para>"
+            flowables.append(Paragraph(paragraph_plan_ejercicio, styles['Normal']))
+            flowables.append(Spacer(1, 12))
+
+            # Construir el PDF
+        document.build(flowables)
+        return "PDF creado"
--- app/Presentation/UserInformationController.py
+++ app/Presentation/UserInformationController.py
@@ -1,6 +1,7 @@
 from app.AppServices.UserInformationServices import UserInformationServices
 from flask_restx import Resource, Namespace, fields
 from injector import inject
+from app.IAServices.IAServices import IAServices
 from app.Presentation.DTO.UserInformationDTO import user_information_dto
 from app.SparkServices.SparkServices import SparkServices
 
@@ -8,39 +9,46 @@ api = Namespace('userinformation', description='User information related operati
 
 UserInformation, GetPredictionResponse = user_information_dto(api)
 
-@api.route('/', strict_slashes=False)
-class UserInformationController(Resource):
+
+""" Este controlador puede servir si tenemos que guardar datos o algo, por mientras esta deprecado y no se utilizad """
+# @api.route('/', strict_slashes=False)
+# class UserInformationController(Resource):
+#     @inject
+#     def __init__(self, user_information_services: UserInformationServices,spark_services: SparkServices, **kwargs):
+#         self.user_information_services = user_information_services
+#         self.spark_services = spark_services
+#         super().__init__(**kwargs)
+
+
+
+
+""" Este controlador es el que se encarga de hacer la predicción de la muerte del paciente """
+@api.route('/analysis', strict_slashes=False)
+class RunAnalysis(Resource):
     @inject
-    def __init__(self, user_information_services: UserInformationServices, **kwargs):
-        self.user_information_services = user_information_services
+    def __init__(self, spark_services: SparkServices, ia_services: IAServices, **kwargs):
+        self.spark_services = spark_services
+        self.ia_services = ia_services
         super().__init__(**kwargs)
 
+    # 
     @api.expect(UserInformation)
     @api.marshal_with(GetPredictionResponse)
     def post(self):
-        death_prediction = self.user_information_services.get_prediction(**api.payload)
+        death_prediction = self.spark_services.get_prediction(**api.payload)
         print("Predicción Controller: ", death_prediction)
-        return {'death_prediction': death_prediction}, 200
+        # TODO: Retornar death_prediction
+        return {'death_prediction': "47%"}, 200
 
-    # @api.expect(UserInformation)
-    # @api.marshal_with(GetPredictionResponse)
-    # def get(self):
-    #     return self.user_information_services.get_prediction(**api.payload)
-
-
-@api.route('/run-analysis', strict_slashes=False)
-class RunAnalysis(Resource):
-    @inject
-    def __init__(self, spark_services: SparkServices, **kwargs):
-        self.spark_services = spark_services
-        super().__init__(**kwargs)
-
+    @api.expect(UserInformation)
     def get(self):
-        result = self.spark_services.run_analysis()
-        return {'result': result}, 200
+        graficas = self.spark_services.get_graficas()
+        healthy_recipes = self.ia_services.get_healthy_recipes(**api.payload)
+        healthy_exercises = self.ia_services.get_healthy_exercises(**api.payload)
 
 
-@api.route('/read-file', strict_slashes=False)
+
+@api.route('/create-pdf', strict_slashes=False)
 class ReadFile(Resource):
     @inject
     def __init__(self, spark_services: SparkServices, **kwargs):
