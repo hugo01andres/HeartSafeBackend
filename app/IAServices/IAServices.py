d92e7c08fdb75609171199ca84bb1c5b4a18f566
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-02-27 17:44:10 +0000

    Quito la variable de time

--- app/IAServices/IAServices.py
+++ app/IAServices/IAServices.py
@@ -9,11 +9,11 @@ class IAServices:
     def __init__(self):
         print("IA.__init__")
     
-    def get_healthy_recipes(self, age, anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine, serum_sodium, sex, smoking, time):
+    def get_healthy_recipes(self, age, anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine, serum_sodium, sex, smoking):
         print("IA.get_healthy_recipes")
         return "Receta 1, Receta 2, Receta 3"
         
-    def get_healthy_exercises(self, age, anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine, serum_sodium, sex, smoking, time):
+    def get_healthy_exercises(self, age, anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine, serum_sodium, sex, smoking):
         print("IA.get_healthy_exercises")
         return "Ejercicio 1, Ejercicio 2, Ejercicio 3"
 
--- app/Presentation/DTO/UserInformationDTO.py
+++ app/Presentation/DTO/UserInformationDTO.py
@@ -8,17 +8,16 @@ class NullableString(fields.String):
 def user_information_dto(api):
     UserInformation = api.model('GetUserInfo', {
         'age': fields.Integer(description='Edad', required=False),
-        'anaemia': fields.Integer(description='Anemia', required=False),
+        'anaemia': fields.Boolean(description='Anemia', required=False),
         'creatine_phosphokinase': fields.Integer(description='Creatine phosphokinase', required=False),
-        'diabetes': fields.Integer(description='Diabetes', required=False),
+        'diabetes': fields.Boolean(description='Diabetes', required=False),
         'ejection_fraction': fields.Integer(description='Ejection fraction', required=False),
-        'high_blood_pressure': fields.Integer(description='High blood pressure', required=False),
-        'platelets': fields.Integer(description='Platelets', required=False),
+        'high_blood_pressure': fields.Boolean(description='High blood pressure', required=False),
+        'platelets': fields.Decimal(description='Platelets', required=False),
         'serum_creatinine': fields.Integer(description='Serum creatinine', required=False),
-        'serum_sodium': fields.Integer(description='Serum sodium', required=False),
+        'serum_sodium': fields.Decimal(description='Serum sodium', required=False),
         'sex': fields.Integer(description='sex', required=False),
-        'smoking': fields.Integer(description='Smoking', required=False),
-        'time': fields.Integer(description='Time', required=False)
+        'smoking': fields.Boolean(description='Smoking', required=False)
     })
     GetPredictionResponse = api.model('GetPredictionResponse', {
         'death_prediction': fields.String(description='Prediction', required=True)
