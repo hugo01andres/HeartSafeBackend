4b125da5114ac3cbfbde3262e0636bae6dc8a404
RaulCdno <91148964+RaulCdno@users.noreply.github.com>
2024-02-28 00:21:33 +0000

    Refactor test.py: Update method calls in test_run_analysis()

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 from app.SparkServices.SparkServices import SparkServices
-from app.IAServices.IAServices import IAServices
 
 def test_run_analysis():
     spark_services = SparkServices()
-    spark_services.get_graficas()
-    # spark_services.probar_test()
-    ia_services = IAServices()
-    ia_services.make_pdf(1, 2, 3)
-    pass
+    spark_services.graficar_columnas()
+    spark_services.guardar_graficos_en_carpeta()
     #spark_services.get_prediction(20, True, 100, True, 20, True, 200, 1, 1)
 
 test_run_analysis()
